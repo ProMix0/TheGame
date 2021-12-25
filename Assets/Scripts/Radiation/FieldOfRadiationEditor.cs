0dae240bdf7f39386e753ab87c0319bf4bb14e56
DocZier <doczier@gmail.com>
2021-12-25 14:10:37 +0300

    ForVideos

--- Assets/Scripts/Radiation/FieldOfRadiationEditor.cs
+++ Assets/Scripts/Radiation/FieldOfRadiationEditor.cs
@@ -1,15 +1,15 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//using UnityEditor;
-// [CustomEditor (typeof (RadiationVisualFieldOfRadiation))]
-//public class FieldOfRadiationEditor : Editor
-//{
-//    void OnSceneGUI()
-//    {
-//        RadiationVisualFieldOfRadiation rad = (RadiationVisualFieldOfRadiation)target;
-//        Handles.color = Color.green;
-//        Handles.DrawWireArc(rad.transform.position, Vector3.up, Vector3.forward, 360, rad.radius);
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+ [CustomEditor (typeof (RadiationVisualFieldOfRadiation))]
+public class FieldOfRadiationEditor : Editor
+{
+    void OnSceneGUI()
+    {
+        RadiationVisualFieldOfRadiation rad = (RadiationVisualFieldOfRadiation)target;
+        Handles.color = Color.green;
+        Handles.DrawWireArc(rad.transform.position, Vector3.up, Vector3.forward, 360, rad.radius);
          
-//    }
-//}
+    }
+}
---
+++ Assets/Scripts/Radiation/WallResist.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallResist : MonoBehaviour
+{
+    [Range(0,1)]
+    public float resistancePersent;
+}
